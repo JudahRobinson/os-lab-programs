d3025195d8733e1e2572ac41a39398cb1b2a951a
Judah Robinson <83941234+JudahRobinson@users.noreply.github.com>
2026-05-12 20:19:48 +0530

    Update fifo.c

--- fifo.c
+++ fifo.c
@@ -1,69 +1,56 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+#include <stdio.h>
 
-\f0\fs24 \cf0 #include <stdio.h>\
-#include <stdlib.h>\
-\
-int pagefault(int a[], int frame[], int n, int no) \{\
-\
-    int i, j = 0, count = 0, k;\
-\
-    for(i = 0; i < no; i++)\
-        frame[i] = -1;\
-\
-    for(i = 0; i < n; i++) \{\
-\
-        int avail = 0;\
-\
-        for(k = 0; k < no; k++) \{\
-\
-            if(frame[k] == a[i])\
-                avail = 1;\
-        \}\
-\
-        if(avail == 0) \{\
-\
-            frame[j] = a[i];\
-\
-            j = (j + 1) % no;\
-\
-            count++;\
-        \}\
-    \}\
-\
-    return count;\
-\}\
-\
-int main() \{\
-\
-    int n, no, fault, i;\
-    int *a, *frame;\
-\
-    printf("ENTER THE NUMBER OF PAGES: ");\
-    scanf("%d", &n);\
-\
-    a = (int *)malloc(n * sizeof(int));\
-\
-    printf("ENTER THE PAGE NUMBER: ");\
-\
-    for(i = 0; i < n; i++)\
-        scanf("%d", &a[i]);\
-\
-    printf("ENTER THE NUMBER OF FRAMES: ");\
-    scanf("%d", &no);\
-\
-    frame = (int *)malloc(no * sizeof(int));\
-\
-    fault = pagefault(a, frame, n, no);\
-\
-    printf("Page Faults: %d\\n", fault);\
-\
-    free(a);\
-    free(frame);\
-\
-    return 0;\
-\}}
+int main()
+{
+    int pages[50], frame[10];
+    int n, f, i, j, k = 0, fault = 0;
+
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
+
+    printf("Enter page reference string:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &pages[i]);
+
+    printf("Enter number of frames: ");
+    scanf("%d", &f);
+
+    for(i = 0; i < f; i++)
+        frame[i] = -1;
+
+    for(i = 0; i < n; i++)
+    {
+        int found = 0;
+
+        for(j = 0; j < f; j++)
+        {
+            if(frame[j] == pages[i])
+            {
+                found = 1;
+                break;
+            }
+        }
+
+        if(found == 0)
+        {
+            frame[k] = pages[i];
+            k = (k + 1) % f;
+            fault++;
+        }
+
+        printf("Frame: ");
+        for(j = 0; j < f; j++)
+        {
+            if(frame[j] != -1)
+                printf("%d ", frame[j]);
+            else
+                printf("- ");
+        }
+
+        printf("\n");
+    }
+
+    printf("Total Page Faults = %d\n", fault);
+
+    return 0;
+}
