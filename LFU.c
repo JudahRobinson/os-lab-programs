13a60f108248825d6b8bed8698f4315c7d6b6eb9
Judah Robinson <83941234+JudahRobinson@users.noreply.github.com>
2026-05-12 20:33:45 +0530

    Update LFU.c

--- LFU.c
+++ LFU.c
@@ -1,74 +1,86 @@
 #include <stdio.h>
 
-int main()
-{
-    int pages[50], frame[10], freq[10];
-    int n, f, i, j, pos, faults = 0;
-    int found, min;
+int main() {
+
+    int i, j, k;
+    int n, frameno;
+
+    int page[50], frame[10];
+    int move = 0, flag;
+    int count = 0;
+
+    int count1[10] = {0};
 
     printf("Enter number of pages: ");
     scanf("%d", &n);
 
-    printf("Enter page reference string:\n");
+    printf("Enter page reference string: ");
+
     for(i = 0; i < n; i++)
-        scanf("%d", &pages[i]);
+        scanf("%d", &page[i]);
 
     printf("Enter number of frames: ");
-    scanf("%d", &f);
+    scanf("%d", &frameno);
 
-    for(i = 0; i < f; i++)
-    {
+    for(i = 0; i < frameno; i++)
         frame[i] = -1;
-        freq[i] = 0;
-    }
 
-    for(i = 0; i < n; i++)
-    {
-        found = 0;
+    printf("Page reference string\tFrames\n");
 
-        for(j = 0; j < f; j++)
-        {
-            if(frame[j] == pages[i])
-            {
-                freq[j]++;
-                found = 1;
+    for(i = 0; i < n; i++) {
+
+        printf("%d\t\t\t", page[i]);
+
+        flag = 0;
+
+        for(j = 0; j < frameno; j++) {
+
+            if(page[i] == frame[j]) {
+
+                flag = 1;
+                count1[j]++;
                 break;
             }
         }
 
-        if(found == 0)
-        {
-            min = freq[0];
-            pos = 0;
+        if(flag == 0) {
 
-            for(j = 1; j < f; j++)
-            {
-                if(freq[j] < min)
-                {
-                    min = freq[j];
-                    pos = j;
+            for(j = 0; j < frameno; j++) {
+
+                if(frame[j] == -1) {
+
+                    frame[j] = page[i];
+                    count1[j]++;
+                    count++;
+
+                    break;
                 }
             }
 
-            frame[pos] = pages[i];
-            freq[pos] = 1;
-            faults++;
+            if(j == frameno) {
+
+                int min = 0;
+
+                for(k = 1; k < frameno; k++) {
+
+                    if(count1[k] < count1[min])
+                        min = k;
+                }
+
+                frame[min] = page[i];
+                count1[min] = 1;
+
+                count++;
+            }
         }
 
-        printf("Frame: ");
-
-        for(j = 0; j < f; j++)
-        {
-            if(frame[j] != -1)
-                printf("%d ", frame[j]);
-            else
-                printf("- ");
-        }
+        for(j = 0; j < frameno; j++)
+            printf("%d\t", frame[j]);
 
         printf("\n");
     }
 
-    printf("Total Page Faults = %d\n", faults);
+    printf("Total Page Faults: %d\n", count);
 
     return 0;
 }
